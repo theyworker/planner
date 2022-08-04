499d76a5211d09869df221931647cece9c799c79
devaka_kum <k.devaka@directfn.com>
2022-08-04 23:11:06 +0530

    layout

--- src/App.js
+++ src/App.js
@@ -8,9 +8,7 @@ import { Container, Typography } from "@mui/material";
 function App() {
   return (
     <Container className="App" sx={{ marginTop: "2em" }}>
-      <Typography variant="h4" component="div" gutterBottom textAlign="center">
-        Employee Manager{" "}
-      </Typography>
+      
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/add" element={<AddEmployee/>} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,28 @@
-import { Box, Container } from '@mui/material';
+import { Box, Button, Container, Paper, Stack, TextField, Typography } from '@mui/material';
 import React from 'react';
 
-const Home = ()=> {
-return <Box>f</Box>
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
+const Today = ()=> {
+return <Box>   <Typography variant="h2" component="div" gutterBottom textAlign="center">
+Today</Typography>
+<Stack spacing={2}>
+        <Item>Item 1</Item>
+        <Item>Item 2</Item>
+        <Item>Item 3</Item>
+      </Stack>
+
+      <TextField id="standard-basic" label="Standard" variant="standard" />
+      <Button variant="outlined">Add</Button>
+</Box>
 }
 
-export default Home;
+export default Today;
