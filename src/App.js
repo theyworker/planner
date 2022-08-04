32268b8770797e72a7838e46bc8ee0ba2a4f5e08
devaka_kum <k.devaka@directfn.com>
2022-08-04 23:05:28 +0530

    set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import { Container, Typography } from "@mui/material";
+// import {AddEmployee} from './components/Home/AddEmployee'
+// import {EditEmployee} from './components/Home/EditEmployee'
+// import { DeleteEmployee } from "./components/Home/DeleteEmployee";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App" sx={{ marginTop: "2em" }}>
+      <Typography variant="h4" component="div" gutterBottom textAlign="center">
+        Employee Manager{" "}
+      </Typography>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/add" element={<AddEmployee/>} />
+        <Route path="/edit/:id" element={<EditEmployee />} />
+        <Route path="/delete/:id" element={<DeleteEmployee />} /> */}
+      </Routes>
+    </Container>
+  
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import { Box, Container } from '@mui/material';
+import React from 'react';
+
+const Home = ()=> {
+return <Box>f</Box>
+}
+
+export default Home;
