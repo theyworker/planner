b28e892c2e9cb3573bc4d3a05a861b56f5c88137
devaka_kum <k.devaka@directfn.com>
2022-08-07 01:34:31 +0530

    show list categories

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,11 @@ import Item from "../subcomponents/Item";
 import SubTitle from "../subcomponents/SubTitle";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import NewListDialog from "../subcomponents/NewListDialog";
+import { useSelector } from "react-redux";
 const Home = () => {
   const [openNewListDialog, setOpenNewListDialog] = useState(false);
+
+  const listCategories = useSelector(state=> state.listCategories.categories || [])
 
   const handleAddClick = () => setOpenNewListDialog(true);
 
@@ -18,6 +21,7 @@ const Home = () => {
         <Grid item xs={6} md={6}>
           <SubTitle text="Lists" />
           <Item item={{ text: "Personal" }} />
+          {listCategories.map(category=> <Item item={{ text: category.name }} />)}
           <AddCircleOutlineIcon
             sx={{ fontSize: 15, paddingTop: "0.5em" }}
             onClick={handleAddClick}
