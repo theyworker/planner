0b0b6e1560418b9d368679d4a835dfef1ee76225
Devaka Kumarasinghe <devaka.kumarasinghe@gmail.com>
2022-11-14 23:13:25 +0530

    clean code

--- planner-services/db.js
+++ planner-services/db.js
@@ -2,20 +2,5 @@ const { mongo_string } = require("./config");
 
 const MongoClient = require("mongodb").MongoClient;
 
-MongoClient.connect(mongo_string, (err, client) => {
-  // ... do something here
-});
-
-exports.getSampleResponse = async () =>
-  MongoClient.connect(mongo_string).then((client) => {
-    console.log("Connected to the DB");
-    // ...
-    const db = client.db("sample");
-    const quotesCollection = db.collection("sample-col");
-
-    return quotesCollection.find({}).toArray();
-    // ...
-  });
-
 exports.executeDB = (callBack) =>
   MongoClient.connect(mongo_string).then(callBack);
